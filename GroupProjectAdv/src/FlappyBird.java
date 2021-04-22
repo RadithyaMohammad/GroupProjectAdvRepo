ed21fdf37501214b68697ce234dcc19fe36eaaf1
radithyamohammad <radithyamohammad@macbook-pro.comfast>
2021-04-22 17:07:30 +0300

    Started flappy bird for 6.2 progress - Radithya

---
+++ GroupProjectAdv/src/FlappyBird.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class FlappyBird {
+	JFrame frame;
+	public static FlappyBird flappybird;
+	public final int WIDTH = 800, HEIGHT = 800;
+
+
+	//constructor
+	public FlappyBird() {
+		frame = new JFrame();
+		
+		frame.setSize(WIDTH, HEIGHT);	
+		frame.setVisible(true);
+		
+		
+	}
+
+	//main method
+	public static void main(String[] args) {
+		flappybird = new FlappyBird();
+	}
+}
